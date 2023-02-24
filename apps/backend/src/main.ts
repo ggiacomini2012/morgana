7d0bd47e40586ed6460ee95693e8be008015f9bd
pro gui <ggiacomini2012@gmail.com>
2023-02-24 15:35:23 -0300

    chore(oh my god!): akarias says: no pode!

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -14,17 +14,16 @@ app.use(cors());
 app.use('/user', routes.user);
 app.use('/agenda', routes.agenda);
 
-const secound = 1000;
-const minute = secound * 60;
-// const hour = minute * 60;
-
-setInterval(async () => {
-  await axios.get(process.env.VITE_APP_API_URL || 'http://localhost:8585');
-}, minute * 5);
-
 app.get('/', (req, res) => res.send('Morgana Api. Routes: /user and /agenda...'));
 
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 app.listen(port, () => {
   console.log(`[ ready ] http://localhost:${port}`);
 });
+
+const secound = 1000;
+const minute = secound * 60;
+// const hour = minute * 60;
+setInterval(async () => {
+  await axios.get(process.env.VITE_APP_API_URL || `http://localhost:${port}`);
+}, minute * 5);
